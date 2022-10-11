777cf059b2094c52cb3f5644a5ce2bd1db8e7977
Rahul Mohan <rahulmohanav35@gmail.com>
2022-10-11 12:24:56 +0530

    time tracking dashboard

---
+++ components/task/Task.js
@@ -0,0 +1,23 @@
+import react from "react";
+import task from "../../styles/Task.module.css";
+function Tasks({ backgroundColor, backgroundImage, todo, duration, weeks }) {
+  return (
+    <div
+      style={{
+        backgroundColor: backgroundColor,
+        backgroundImage: `url(${backgroundImage})`,
+      }}
+      className={task.seconddiv}
+    >
+      <div className={task.innerdiv}>
+        <div className={task.workdiv}>
+          <h1 className={task.work}> {todo}</h1>
+          <img className={task.image} src="/assets/images/icon-ellipsis.svg" />
+        </div>
+        <h1 className={task.time}>{duration}hrs</h1>
+        <h2 className={task.week}>Last week-{weeks}hrs</h2>
+      </div>
+    </div>
+  );
+}
+export default Tasks;
